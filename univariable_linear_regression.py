1c002e1a95b1da684b05964c60a02677020fb089
clone <umarbektokyo@icloud.com>
2023-04-29 11:51:55 +0900

    Gradient descent and example is written

--- univariable_linear_regression.py
+++ univariable_linear_regression.py
@@ -16,7 +16,6 @@ def compute_cost(x, y, w, b):
         cost = (y[i] - f_wb) ** 2
         cost_sum += cost
     total_cost = (1 / (2 * m)) * cost_sum
-
     return total_cost
 
 # Gradient function for univariable linear regression
@@ -32,7 +31,26 @@ def compute_gradient(x, y, w, b):
         dj_db += dj_db_i
     dj_dw = dj_dw / m
     dj_db = dj_db / m
-
     return dj_dw, dj_db
 
 # Gradient descent function for univariable linear regression
+def gradient_descent(x, y, w_in, b_in, alpha, num_iters, gradient_function):
+    b = b_in
+    w = w_in
+    for i in range(num_iters):
+        dj_dw, dj_db = gradient_function(x, y, w, b)
+        b = b - alpha * dj_db
+        w = w - alpha * dj_dw
+    return w, b
+
+def example():
+    x_example = np.array([1.0, 1.7, 2.0, 2.5, 3.0, 3.2,])
+    y_example = np.array([250, 300, 480, 430, 630, 730,])
+    w_init = 0
+    b_init = 0
+    iterations = 10000
+    alpha_init = 1.0e-2
+    w_final, b_final = gradient_descent(x_example, y_example, w_init, b_init, alpha_init, iterations, gradient_descent)
+    print(f"The final function: {w_final:8.4f} * x + {b_final:8.4f}")
+
+example()
